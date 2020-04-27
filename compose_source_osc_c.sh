c9a7c9f898ad9c9d0a737237ca2de1e1189ac3d5
murray foster <mrafoster@gmail.com>
2020-04-27 02:24:55 +0000

    refactored parameter system, modules can now be loaded dynamically from 'modules/' directory  -- existing modules need to be moved over  -- new varslope lowpass filter needs to be functionally tested

---
+++ compose_source_osc_c.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cp templates/osc.c.template/osc.c.template.prefix osc.c
+
+for module_type in `ls modules/ | grep -v "\."`
+do
+    for module_subtype in `ls modules/$module_type/ | grep -v "\."`
+    do
+	for module in `ls modules/$module_type/$module_subtype/ | grep -v "\."`
+	do
+	    osc_params=`cat "modules/$module_type/$module_subtype/$module/osc.params"`
+	    add_func=`cat "modules/$module_type/$module_subtype/$module/osc_modules_${module_type}_${module_subtype}_${module}.h" | grep add | cut -d' ' -f2 | cut -d'(' -f1`
+	    edit_func=`cat "modules/$module_type/$module_subtype/$module/osc_modules_${module_type}_${module_subtype}_${module}.h" | grep edit | cut -d' ' -f2 | cut -d'(' -f1`
+	    echo "#include \"modules/$module_type/$module_subtype/$module/osc_modules_${module_type}_${module_subtype}_${module}.h\"" >> osc.c
+	done
+    done
+done
+
+cat templates/osc.c.template/osc.c.template.mid.0 >> osc.c
+
+for module_type in `ls modules/ | grep -v "\."`
+do
+    for module_subtype in `ls modules/$module_type/ | grep -v "\."`
+    do
+	for module in `ls modules/$module_type/$module_subtype/ | grep -v "\."`
+	do
+	    osc_params=`cat "modules/$module_type/$module_subtype/$module/osc.params"`
+	    add_func=`cat "modules/$module_type/$module_subtype/$module/osc_modules_${module_type}_${module_subtype}_${module}.h" | grep add | cut -d' ' -f2 | cut -d'(' -f1`
+	    edit_func=`cat "modules/$module_type/$module_subtype/$module/osc_modules_${module_type}_${module_subtype}_${module}.h" | grep edit | cut -d' ' -f2 | cut -d'(' -f1`
+	    osc_path="/cyperus/add/module/$module_type/$module_subtype/$module"
+	    handler_prefix="\"$osc_path\", \"$osc_params\""
+	    echo "  lo_server_thread_add_method(lo_thread, $handler_prefix, $add_func, NULL);" >> osc.c
+	    echo "  lo_server_thread_add_method(lo_thread, $handler_prefix, $edit_func, NULL);" >> osc.c
+	done
+    done
+done
+cat templates/osc.c.template/osc.c.template.suffix >> osc.c
+
