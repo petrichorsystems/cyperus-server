0b67bb64babacb86e95af61c7f2962fed25cf190
resinbeard <mrafoster@gmail.com>
2023-12-05 11:24:22 -0800

    converting math_modules_oscillator_sine() to block processing flow

--- src/modules/oscillator/sine/math_modules_oscillator_sine.c
+++ src/modules/oscillator/sine/math_modules_oscillator_sine.c
@@ -3,19 +3,23 @@
 
 #include "math_modules_oscillator_sine.h"
 
-extern
-float math_modules_oscillator_sine(dsp_module_parameters_t *parameters, int samplerate, int pos) {
-  float frequency = parameters->float32_type[0];
-  float amplitude = parameters->float32_type[1];
-  float phase = parameters->float32_type[2];
+extern float* math_modules_oscillator_sine(dsp_module_parameters_t *parameters, int samplerate) {
+  float *out = malloc(sizeof(float) * dsp_global_period);
   
-  float phase_delta = parameters->float32_type[3];
+  float *frequency = parameters->float32_arr_type[0];
+  float *amplitude = parameters->float32_arr_type[1];
+  float *phase = parameters->float32_arr_type[2];
   
-  phase_delta += 2.0f * M_PI * frequency * (1.0f/samplerate) + phase;
-  while( phase_delta > 2.0f * M_PI )
-    phase_delta -= 2 * M_PI;
+  float phase_delta = parameters->float32_type[0];
 
-  parameters->float32_type[3] = phase_delta;
+  for(int p=0; p<dsp_global_period; p++) {
+    phase_delta += 2.0f * M_PI * frequency[p] * (1.0f/samplerate) + phase[p];
+    while( phase_delta > 2.0f * M_PI )
+      phase_delta -= 2 * M_PI;
+    out[p] = sin(phase_delta) * amplitude[p];
+  }
+  parameters->float32_type[0] = phase_delta;
 
-  return sin(phase_delta) * amplitude;
+  
+  return out;
 }
