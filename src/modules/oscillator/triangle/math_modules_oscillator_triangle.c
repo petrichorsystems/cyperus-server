d6ea840665d9aeed2b518e23e04712340c9c94c1
resinbeard <mrafoster@gmail.com>
2024-02-13 15:06:33 -0800

    fixed triangle wave calculation in module oscillator/triangle

--- src/modules/oscillator/triangle/math_modules_oscillator_triangle.c
+++ src/modules/oscillator/triangle/math_modules_oscillator_triangle.c
@@ -13,10 +13,9 @@ extern float* math_modules_oscillator_triangle(dsp_module_parameters_t *paramete
 	int x = parameters->int32_type[0];
 	
 	for (int p = 0; p<dsp_global_period; p++) {
-		if (x >= samplerate) {
+		if (x >= (int)(samplerate / frequency[p]))
 			x = 0;
-		}
-		out[p] = ((2 * amplitude[p]) / M_PI) * asinf( sinf( 2 * M_PI * (float)x/samplerate * frequency[p]) );
+		out[p] = ((2 * amplitude[p]) / M_PI) * asinf( sinf( 2 * M_PI * (float)x/(samplerate / frequency[p])));
 		x += 1;
 	}
 	parameters->int32_type[0] = x;
